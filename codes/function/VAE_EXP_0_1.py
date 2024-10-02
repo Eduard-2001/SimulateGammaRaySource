daae0249093d624d63d253fe927f2b0157313355
xingwei <201840319@smail.nju.edu.cn>
2024-10-02 17:51:50 +0800

    debug of jsdivergence

--- codes/function/VAE_EXP_0_1.py
+++ codes/function/VAE_EXP_0_1.py
@@ -6,14 +6,14 @@ import torch.nn.functional as F
 class Encoder(nn.Module):
     def __init__(self, latent_dim):
         super(Encoder, self).__init__()
-        self.conv1 = nn.Conv2d(1, 4, kernel_size=3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(4, 16, kernel_size=3, stride=2, padding=1)
+        #self.conv1 = nn.Conv2d(1, 4, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
         self.conv3 = nn.Conv2d(16, 64, kernel_size=3, stride=2, padding=1)
         self.fc_mu = nn.Linear(64 * 16 * 16, latent_dim) 
         self.fc_logvar = nn.Linear(64 * 16 * 16, latent_dim) 
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
+        #x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         x = x.view(x.size(0), -1)
@@ -26,15 +26,15 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         self.fc = nn.Linear(latent_dim, 64 * 16 * 16)
         self.conv3 = nn.ConvTranspose2d(64, 16, kernel_size=4, stride=2, padding=1, output_padding=0)
-        self.conv2 = nn.ConvTranspose2d(16, 4, kernel_size=4, stride=2, padding=1, output_padding=0)
-        self.conv1 = nn.ConvTranspose2d(4, 1, kernel_size=4, stride=2, padding=1, output_padding=0)
+        self.conv2 = nn.ConvTranspose2d(16, 1, kernel_size=4, stride=2, padding=1, output_padding=0)
+        #self.conv1 = nn.ConvTranspose2d(4, 1, kernel_size=4, stride=2, padding=1, output_padding=0)
 
     def forward(self, x):
         x = self.fc(x)
         x = x.view(x.size(0), 64, 16, 16)
         x = F.relu(self.conv3(x))
-        x = F.relu(self.conv2(x))
-        x = torch.sigmoid(self.conv1(x))
+        x = torch.sigmoid(self.conv2(x))
+        #x = torch.sigmoid(self.conv1(x))
         return x
 
 class VAE(nn.Module):
@@ -42,12 +42,20 @@ class VAE(nn.Module):
         super(VAE, self).__init__()
         self.encoder = Encoder(latent_dim)
         self.decoder = Decoder(latent_dim)
+        self.reset_parameters()
         
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         z = mu + eps * std
         return z
+
+    def reset_parameters(self):
+        nn.init.xavier_normal_(self.encoder.conv2.weight)
+        nn.init.xavier_normal_(self.encoder.conv3.weight)
+        nn.init.xavier_normal_(self.decoder.conv2.weight)
+        nn.init.xavier_normal_(self.decoder.conv3.weight)
+        return 
     
     def forward(self, x):
         mu, logvar = self.encoder(x)
